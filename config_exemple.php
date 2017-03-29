f18ab1a020184f4590ff0c3836b1f140561d5b3f
Slowdream <slowdream@yandex.ru>
2017-03-29 11:48:21 +0300

    Добавлен gitignore и вынесен за рамки проекта файл config.php

--- config_exemple.php
+++ config_exemple.php
@@ -6,14 +6,14 @@ setlocale(LC_ALL, 'ru_RU.UTF8');
 ignore_user_abort(true);
 
 //Database
-define('MYSQL_SERVER', '127.0.0.1:3305');
+define('MYSQL_SERVER', '127.0.0.1');
 define('MYSQL_USER', 'root');
-define('MYSQL_PASSWORD', '');
-define('MYSQL_DB', 'myparser');
+define('MYSQL_PASSWORD', '****');
+define('MYSQL_DB', 'dbname');
 
 //Yandex
-define('YA_USER', 'slowdream');
-define('YA_KEY', '03.37625579:d507e2531451fe7594804a149af15de0');
+define('YA_USER', '');
+define('YA_KEY', '');
 define('YA_FILTER', 'moderate'); // Фильтр: strict - семеный, moderate - умеренная фильтрация, none - отключен.
 
 define('VIEWER_DIR', 'data/viewer_stat/');
